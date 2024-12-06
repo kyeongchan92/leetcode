50f473356b4952352419e7f871fff6a2e81d59f5
kyeongchan92 <nahcklee@gmail.com>
2024-12-06 14:14:48 +0900

    Time: 82 ms (13.84%), Space: 17.5 MB (5.3%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,12 +3,9 @@ class Solution:
         collect = []
         max_len = 0
         for i, alphabet in enumerate(s):
-            print(f"{alphabet}".ljust(60, '-'))
             if alphabet not in collect:
-                print(f"if")
                 collect.append(alphabet)
                 max_len = max(max_len, len(collect))
-                print(f"collect : {collect}, max_len = {max_len}")
             else:
                 restart_idx = len(collect) - 1
                 while collect[restart_idx] != alphabet:
