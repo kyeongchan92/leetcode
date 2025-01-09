cd1b7e09efb7c273453439a264a341e72596cf59
kyeongchan92 <nahcklee@gmail.com>
2025-01-09 23:01:07 +0900

    Time: 9083 ms (5%), Space: 17.8 MB (34.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,9 +4,7 @@ class Solution:
         max_pel_str = ''
         for i in range(len(s)+1):
             for j in range(i+1, len(s)+1):
-                if s[i:j] == s[i:j][::-1]:
-                    length = len(s[i:j])
-                    if length > max_length:
-                        max_pel_str = s[i:j]
-                        max_length = length
+                if s[i:j] == s[i:j][::-1] and len(s[i:j]) > max_length:
+                    max_pel_str = s[i:j]
+                    max_length = len(s[i:j])
         return max_pel_str
